a899cb66f80fa5c6d6a2d205f4439a7237875f99
HoChiMinhSama69 <hwng6912@gmail.com>
2018-09-23 21:09:09 -0700

    riptrandaiquang

---
+++ riptrandaiquang.py
@@ -0,0 +1,11 @@
+n=input("username: ")
+print(n)
+if n=="techkids":
+    m=input("password: ")
+    print(m)
+    if m=="codethechange":
+        print("welcome superuser")
+    else:
+        print("invalid password")
+else:
+    print("you are not superuser")
