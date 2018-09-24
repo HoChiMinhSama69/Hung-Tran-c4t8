af60e4248ae85cb07d7b34a399603450b5818ecd
HoChiMinhSama69 <hwng6912@gmail.com>
2018-09-23 21:23:34 -0700

    thang

---
+++ thang.py
@@ -0,0 +1,12 @@
+n=int(input("month? "))
+print(n)
+if n < 4:
+    print("XUAN")
+elif n<8:
+    print("HE")
+elif n<10:
+    print("THU")
+elif n<13:
+    print("DONG")
+else: 
+    print("DEO CO THANG DAY THANG NGU")
