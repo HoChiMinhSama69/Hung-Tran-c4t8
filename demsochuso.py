304f86d6ccc5fc186c53dfedef06dfc4f511f09a
HoChiMinhSama69 <hwng6912@gmail.com>
2018-09-29 20:33:00 -0700

    5lan

---
+++ demsochuso.py
@@ -0,0 +1,7 @@
+while True:
+    n=input("HAY NHAP 1 SO VAO: ")
+    if n.isdigit():
+        print("SO CUA BAN CO",len(n),"CHU SO")
+        break
+    else:
+        print("DAY DEO PHAI LA SO :0")
